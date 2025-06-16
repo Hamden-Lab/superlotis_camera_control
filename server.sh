f4654ff49831550bbc00c88f0d8f6712e7187fbe
Jacob Vider <60020199+jacobvider@users.noreply.github.com>
2025-06-16 16:30:44 -0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# # Navigate to the directory
-# cd /opt/PrincetonInstruments/picam/samples/server-client || {
-#   echo "Directory not found!"
-#   exit 1
-# }
+# Navigate to the directory
+cd /opt/PrincetonInstruments/picam/samples/server-client || {
+  echo "Directory not found!"
+  exit 1
+}
 
-# # Run make
-# echo "Running make..."
-# make || {
-#   echo "Make failed!"
-#   exit 1
-# }
+# Run make
+echo "Running make..."
+make || {
+  echo "Make failed!"
+  exit 1
+}
 
 # Start the camera server
 echo "Starting camserver_cit..."
